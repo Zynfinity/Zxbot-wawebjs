37ed414317c51af3621286e64cf0267f29c27d52
fajar <67202122+Zynfinity@users.noreply.github.com>
2022-02-27 19:08:34 +0700

    p

--- commands/runtime.js
+++ commands/runtime.js
@@ -1,7 +1,7 @@
 const {toTimer} = require('../lib/tools')
 module.exports = {
 	name: ['runtime'].map((v) => v + ''),
-	cmd: ['runtime'],
+	cmd: /^(runtime)$/i,
 	category: 'other',
 	async handler(m, {conn}){
 		conn.reply(m.from, `Runtime : ${await toTimer(process.uptime())}`, m.id)
---
+++ commands/snapshot.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: ['snapshot'].map((v) => v + ''),
+	cmd: /^(snap|snapshot)$/i,
+	category: 'owner',
+owner: true,
+	async handler(m, {conn}){
+		conn.getSnapshot().then(res => conn.sendImage(m.from, res, '', 'Snap Session'))
+	}
+}
