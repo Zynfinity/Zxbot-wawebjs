af1d094bb01ad45686f9a582e68c9322aa33c56e
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2022-05-10 10:51:37 +0000

    ya

--- index.js
+++ index.js
@@ -11,56 +11,92 @@ const {
     owner
 } = require('./lib/config');
 const toms = require('ms');
+const { sleep } = require('./lib/tools')
 const databes = JSON.parse(fs.readFileSync('./lib/json/data.json'))
+const startt = async() => {
+    start()
+}
 async function start(){
     await console.log(chalk.green('Welcome To ZXBOT'))
     console.log(chalk.green('Please wait, Starting Bot...'))
-const client = await new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-gpu-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-features=IsolateOrigins,site-per-process',
-          ],
-        exitOnPageError: false,
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-    }
-});
-return client
-}
-start().then(async client => {
-client.on('change_state', msg => console.log(msg))
-client.on('auth_failure', msg => console.log(msg))
-client.on('disconnected', async dc => {
-    console.log('Bot Disconnected')
-    await client.destroy()
-    start()
-})
-client.initialize().then(async re => {
-    await require('./lib/interval')(client)
-})
-client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-    qrcode.generate(qr, {
-        small: true
-    }, function(qrcode) {
-        console.log(qrcode)
+    const client = await new Client({
+        authStrategy: new LocalAuth({
+            dataPath: `./zxbot_data`
+        }),
+        puppeteer: {
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-gpu-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-features=IsolateOrigins,site-per-process',
+            ],
+            exitOnPageError: false,
+        }
     });
-    console.log('Scan Qr Code');
-});
-client.on('ready', async () => {
-    require('./lib/database/database').connectToDatabase()
-    console.log('Bot is ready!');
-    console.log('Whatsapp-Web Version : ' + await client.getWWebVersion())
-    client.sendMessage(owner, JSON.stringify(client.info, null, 2))
-    databes.restarttime = Date.now() + await toms('7h')
-    databes.cleartime = Date.now() + await toms('1h')
-    fs.writeFileSync('./lib/json/data.json', JSON.stringify(databes))
-});
-client.on('auth_failure', msg => console.log(msg))
+    client.on('change_state', msg => console.log(msg))
+    client.on('auth_failure', msg => console.log(msg))
+    client.initialize().then(async re => {
+        await require('./lib/interval')(client)
+    })
+    client.on('qr', (qr) => {
+        // Generate and scan this code with your phone
+        qrcode.generate(qr, {
+            small: true
+        }, function(qrcode) {
+            console.log(qrcode)
+        });
+        console.log('Scan Qr Code');
+    });
+    client.on('ready', async () => {
+        require('./lib/database/database').connectToDatabase()
+        console.log('Bot is ready!');
+        console.log('Whatsapp-Web Version : ' + await client.getWWebVersion())
+        client.sendMessage(owner, JSON.stringify(client.info, null, 2))
+        databes.restarttime = Date.now() + await toms('7h')
+        databes.cleartime = Date.now() + await toms('1h')
+        fs.writeFileSync('./lib/json/data.json', JSON.stringify(databes))
+    });
+    client.on('auth_failure', msg => console.log(msg))
+    client.on('message', async msg => {
+        client.msgdata = client.msgdata ? client.msgdata : []
+        if (msg.type == 'chat' || msg.type == 'image' || msg.type == 'video' || msg.type == 'list_response') {
+            if (client.msgdata.length > 50) client.msgdata = []
+            if (msg.type == 'list_response' || msg.body.startsWith('.')) {
+                //coman = await Object.values(global.commands).find((rescmd) => !rescmd.function && !rescmd.disabled && rescmd.cmd.includes(msg.type === 'list_response' ? msg.selectedRowId.split(' ')[0].replace('.', '') : msg.body.split(' ')[0].replace('.', '')))
+                //if(coman != undefined){
+                    client.msgdata.push({
+                        caption: msg.type === 'list_response' ? msg.selectedRowId : msg.body,
+                        msgId: msg.id._serialized,
+                        sender: msg.id.remote.endsWith('@g.us') ? msg.author : msg.from
+                    })
+                //}
+                await require('./lib/handler').handler(msg, client)
+            }
+        }
+        await require('./lib/function').handler(msg, client)
+    });
+    client.on('group_update', async upt => {
+        await require('./events/group_update')(upt, client)
+    })
+    client.on('group_join', async upt => {
+        await require('./events/greetings').welcome(upt, client)
+    })
+    client.on('group_leave', async upt => {
+        await require('./events/greetings').left(upt, client)
+    })
+    client.on('incoming_call', async call => {
+        await require('./events/call.js')(call, client)
+    })
+    client.on('disconnected', async dc => {
+        console.log(dc)
+        console.log('Bot Disconnected')
+        await client.destroy()
+        await sleep(5000)
+        startt()
+    })
+    return client
+}
 //read command
 let pluginFolder = path.join(__dirname, 'commands')
 let pluginFilter = (filename) => /\.js$/.test(filename)
@@ -102,37 +138,6 @@ global.reload = (_event, filename) => {
 }
 Object.freeze(global.reload)
 fs.watch(path.join(__dirname, 'commands'), global.reload)
-client.on('message', async msg => {
-    client.msgdata = client.msgdata ? client.msgdata : []
-    if (msg.type == 'chat' || msg.type == 'image' || msg.type == 'video' || msg.type == 'list_response') {
-        if (client.msgdata.length > 50) client.msgdata = []
-        if (msg.type == 'list_response' || msg.body.startsWith('.')) {
-            //coman = await Object.values(global.commands).find((rescmd) => !rescmd.function && !rescmd.disabled && rescmd.cmd.includes(msg.type === 'list_response' ? msg.selectedRowId.split(' ')[0].replace('.', '') : msg.body.split(' ')[0].replace('.', '')))
-            //if(coman != undefined){
-                client.msgdata.push({
-                    caption: msg.type === 'list_response' ? msg.selectedRowId : msg.body,
-                    msgId: msg.id._serialized,
-                    sender: msg.id.remote.endsWith('@g.us') ? msg.author : msg.from
-                })
-            //}
-            await require('./lib/handler').handler(msg, client)
-        }
-    }
-    await require('./lib/function').handler(msg, client)
-});
-client.on('group_update', async upt => {
-    await require('./events/group_update')(upt, client)
-})
-client.on('group_join', async upt => {
-    await require('./events/greetings').welcome(upt, client)
-})
-client.on('group_leave', async upt => {
-    await require('./events/greetings').left(upt, client)
-})
-client.on('incoming_call', async call => {
-    await require('./events/call.js')(call, client)
-})
-})
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
     fs.unwatchFile(file)
@@ -140,3 +145,4 @@ fs.watchFile(file, () => {
     delete require.cache[file]
     if (global.reload) console.log(global.reload())
 })
+startt()
